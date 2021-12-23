ef37eb19c9c866aceb80abe07c3cf3be903a8ac8
User <prathiksha07416@gmail.com>
2021-12-23 21:51:29 +0530

    Any positive integer of n digits is Armstrong number,if sum of the nth power of each digit equals to the number itself.

--- armstrong.py
+++ armstrong.py
@@ -1,1 +1,20 @@
-print("Armstrong number")
+number=int(input("Enter any positive number : "))
+def check_armstrong(number):
+    if number in range(1,10):
+        return True
+    order=len(str(number))
+    sum=0
+    original=number
+    while number>0:
+        digit=number%10
+        sum+=digit**order
+        number=number//10
+    if sum==original:
+        return True
+    return False
+if check_armstrong(number):
+    print('Number is Armstrong')
+else:
+    print('Number is not Armstrong')
+
+
