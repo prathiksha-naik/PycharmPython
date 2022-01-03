ea2ef079408b1134a28a2abf61bbaeb2329889f4
prathiksha07416 <75781671+prathiksha07416@users.noreply.github.com>
2022-01-03 21:47:58 +0530

    Program to greet all the person names in list starting with letter P.

---
+++ List.py
@@ -0,0 +1,6 @@
+#Write a program to greet all the person names in list starting with letter P
+l1=["Prathiksha","Prarthana","Samiksha","Pratham"]
+for name in l1:
+    if name.startswith("P"):
+        print(name)
+    
