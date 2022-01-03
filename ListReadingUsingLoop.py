4e4c938048992dace626d0f8054df6e0b1d41b69
prathiksha07416 <75781671+prathiksha07416@users.noreply.github.com>
2022-01-03 21:45:52 +0530

    Print item in list using while loop and for loop.

---
+++ ListReadingUsingLoop.py
@@ -0,0 +1,12 @@
+#Print item in list using while loop
+fruits=["Mango","Kiwi","Orange","Grapes"]
+i=0
+while i<len(fruits):
+    print(fruits[i])
+    i=i+1
+
+#Print item in list using for loop
+fruits=["Jackfruit","Watermelon","Apple","Banana"]
+for i in fruits:
+    print(i)
+
