06f60413086ed14157400222b9b3239c63e63210
prathiksha07416 <75781671+prathiksha07416@users.noreply.github.com>
2022-01-04 22:44:09 +0530

    Write a program to remove characters from a string starting from zero up to n and return a new string.
    For example:
    remove_chars("pynative", 4) so output must be tive. Here we need to remove first four characters from a string.
    remove_chars("pynative", 2) so output must be native. Here we need to remove first two characters from a string.

---
+++ RemoveCharString.py
@@ -0,0 +1,9 @@
+#Write a program to remove characters from a string starting from zero up to n and return a new string.
+def remove_chars(word, n):
+    print('Original string:', word)
+    x = word[n:]
+    return x
+
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+print(remove_chars("pynative", 2))
