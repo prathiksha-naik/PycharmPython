d15129b50581e1999896b97fd0b3983e7b99120e
prathiksha07416 <75781671+prathiksha07416@users.noreply.github.com>
2022-01-06 22:43:42 +0530

    Capitalize first and last character of the string.

---
+++ String.py
@@ -0,0 +1,10 @@
+#Capitalize first and last character of the string
+string="prathiksha"
+s=string[0].upper()+string[1:-2]+string[-1].upper()
+print(s)
+
+def num_dig(string):
+    if i.isalpha():
+        flag_1=True
+    elif i.isdigit():
+        flag_2=False
