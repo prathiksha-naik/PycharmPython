e63352915f3fa670564261a5264b93f5ec7899d7
User <prathiksha07416@gmail.com>
2022-01-09 11:23:23 +0530

    Given string contains a combination of the lower and uppeer case letters.Write a program to arrange the characters of a string so that all lowercase letters should come first.

---
+++ Arrangestring.py
@@ -0,0 +1,13 @@
+"""Given string contains a combination of the lower and upper case letters. 
+Write a program to arrange the characters of a string so that all lowercase letters should come first."""
+string = "PSNprathi"
+lower = []
+upper = []
+for strings in string:
+    if strings.islower():
+        lower.append(strings)
+    else:
+        upper.append(strings)
+combstring = ''.join(lower + upper)
+print(str(combstring))
+
---
+++ firstmidlaststring.py
@@ -0,0 +1,24 @@
+str1 = 'James'
+print("Original String is", str1)
+
+# Get first character
+res = str1[0]
+
+# Get string size
+l = len(str1)
+print("Length of string: ", l)
+print("First character of string:",res)
+# Get middle index number
+mi = int(l / 2)
+middle = str1[mi]
+print("Middle character of string: ", middle)
+# Get middle character and add it to result
+res = res + middle
+
+last = str1[l - 1]
+print("Last character of string: ", last)
+
+# Get last character and add it to result
+res = res + last
+
+print("New String:", res)
